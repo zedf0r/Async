8b4ba7ccb1c2c035f2395e452e81ac22805ad1f6
Leonid <zemlyanykh@bk.ru>
2024-11-22 16:05:42 +0500

    Добавлен метод try/catch

--- src/js/gameSaving.js
+++ src/js/gameSaving.js
@@ -3,8 +3,13 @@ import read from "./reader";
 
 export class GameSavingLoader {
     static async load() {
-        const data = await read();
-        const value = await json(data);
-        return JSON.parse(value);
+        try {
+            const data = await read();
+            const value = await json(data);
+            return JSON.parse(value);
+        } catch (error) {
+            throw new Error(error)
+        }
+        
     }
 }
